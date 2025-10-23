f84ce12bcfe0873612dd7a65ddd0e05bcef935f6
gbarbosa06 <andradegui2006@gmail.com>
2025-10-22 22:06:11 -0300

    Base do projeto + alguns arquivos de model

---
+++ src/main/java/model/Hospital.java
@@ -0,0 +1,57 @@
+package model;
+
+public class Hospital {
+    private long id;
+    private String nome;
+    private String cnpj;
+    private Endereco endereco;
+    private String telefone;
+
+    public Hospital(Long id, String nome, String cnpj, Endereco endereco, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/model/Paciente.java
@@ -0,0 +1,96 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Paciente {
+    private long id;
+    private Hospital hospital; //A referência do hospital, ao inves de só o id como no banco
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private String telefone;
+    private String email;
+    private Endereco endereco;
+
+    public Paciente(long id,
+                    Hospital hospital,
+                    String nome,
+                    String cpf,
+                    LocalDate dataNascimento,
+                    String telefone,
+                    String email,
+                    Endereco endereco) {
+        this.id = id;
+        this.hospital = hospital;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.telefone = telefone;
+        this.email = email;
+        this.endereco = endereco;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Hospital getHospital() {
+        return hospital;
+    }
+
+    public void setHospital(Hospital hospital) {
+        this.hospital = hospital;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
